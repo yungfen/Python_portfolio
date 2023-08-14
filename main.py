c17edec3b578800f29320b58a4b477af206ab19a
fen <yungfen8911@gmail.com>
2023-08-14 16:33:30 +0800

    Create columns, image and description

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+import streamlit as st
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+st.set_page_config(layout="wide")
 
+col1, col2 = st.columns(2)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+with col1:
+    st.image("images/photo2.jpg", width=500)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with col2:
+    st.title("Tiffany Liu")
+    content = """
+    Hi, I am Tiffany. I am from Taiwan, and I am currently pursuing a double-major degree of CS. 
+    I am learning Python, and here is my portfolio of my Python project. 
+    \n Welcome!"""
+    st.info(content)

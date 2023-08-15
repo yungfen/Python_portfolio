eb871cad33fb6fff2ca73f3b211289149e57ab85
fen <yungfen8911@gmail.com>
2023-08-15 18:32:41 +0800

    Day 22: Render the titles in two columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -17,3 +18,14 @@ content2 = """Below you can find my projects I worked on.
 Feel free to contact me."""
 
 st.write(content2)
+
+df = pandas.read_csv("data.csv", sep=";")
+col3, col4 = st.columns(2)
+with col3:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
